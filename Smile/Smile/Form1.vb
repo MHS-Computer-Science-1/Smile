6f3099d60eac10db5c722c79970c0f5a86648a4e
foxrw1 <78601276+foxrw1@users.noreply.github.com>
2025-10-28 11:44:15 -0400

    Add files via upload

---
+++ Smile/Smile/Form1.vb
@@ -0,0 +1,30 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        '1. Create a 2D array called 'grid' with 10 rows and 10 columns
+
+        '2. Run the app, click the button and make sure it displays 100 zeros
+
+
+        '3. Set every element to 1 using loops
+
+
+        '4. Run the app, verify that 1s are now displayed
+
+
+        '5. Now, set some of the elements to 8 to create a smiley face
+        '   as shown in the lab description
+
+
+
+        'This code displays the array in the ListBox
+        'Do not alter it
+        For row = 0 To grid.GetLength(0) - 1
+            Dim rowOut As String = ""
+            For col = 0 To grid.GetLength(1) - 1
+                rowOut = rowOut & grid(row, col) & " "
+            Next
+            ListBox1.Items.Add(rowOut)
+        Next
+
+    End Sub
+End Class
